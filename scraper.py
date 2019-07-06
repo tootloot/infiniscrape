78214c5d7ec2e5e25ea2c1527a948dc4816e8427
tootloot <tootloot@gmail.com>
2019-07-05 20:52:02 -0700

    Scrape all pages into list of dicts with timestamp and string of mod action

--- scraper.py
+++ scraper.py
@@ -3,7 +3,37 @@ import urllib
 import time
 from bs4 import BeautifulSoup
 
+def processmodactions(soup):
+    rawmodactions = soup.find("table", {"class": "modlog"}).contents[1:]
+    modactions = []
+    for line in rawmodactions:
+        linedict = {}
+        timestamp = line.contents[2].find("span").attrs.get("title")
+        description = line.contents[4].text
+        linedict["timestamp"] = timestamp
+        linedict["description"] = description
+        modactions.append(linedict)
+    return modactions
+
+def listoflinks(soup):
+    result = []
+    otherlinks = soup.find("p", {"class": "unimportant"})
+    for link in list(filter(lambda x: x.name == 'a', otherlinks.contents)):
+        test = url + link.attrs.get('href')
+        result.append(test)
+    return result
+
+
 url = 'https://8ch.net/log.php?board=pol'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
-soup.findAll('a')
+pagelist = processmodactions(soup)
+otherpages = listoflinks(soup)
+for page in otherpages:
+    url = page
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    pagelist += processmodactions(soup)
+print("mod actions: " + str(len(pagelist)) + ", first timestamp: " + pagelist[0].get("timestamp") + "last timestamp: " + pagelist[-1].get("timestamp"))
+
+
