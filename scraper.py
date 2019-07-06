d3700b092d6f0ea127fef8822b96083a4348d60d
tootloot <tootloot@gmail.com>
2019-07-06 00:52:47 -0700

    Scraping to file working and tested

--- scraper.py
+++ scraper.py
@@ -3,8 +3,8 @@ import urllib
 import time
 from bs4 import BeautifulSoup
 
-def processmodactions(soup):
-    rawmodactions = soup.find("table", {"class": "modlog"}).contents[1:]
+def processmodactions(modactionslist):
+    rawmodactions = modactionslist.find("table", {"class": "modlog"}).contents[1:]
     modactions = []
     for line in rawmodactions:
         linedict = {}
@@ -19,9 +19,27 @@ def listoflinks(soup):
     result = []
     otherlinks = soup.find("p", {"class": "unimportant"})
     for link in list(filter(lambda x: x.name == 'a', otherlinks.contents)):
-        test = url + link.attrs.get('href')
+        test = 'https://8ch.net/log.php' + link.attrs.get('href')
         result.append(test)
     return result
+
+def appendtofile(buffer, filename):
+    with open(filename, "a") as outputfile:
+        outputfile.write(buffer)
+
+def processlinks(linkstoprocess, pagelist):
+    for page in otherpages:
+        url = page
+        print(url)
+        loopresponse = requests.get(url)
+        loopsoup = BeautifulSoup(loopresponse.text, "html.parser")
+        pagedata = processmodactions(loopsoup)
+        buffer = ""
+        for line in pagedata:
+            buffline = line.get("timestamp") + ": " + line.get("description") + "\n"
+            buffer += buffline
+        appendtofile(buffer, filename)
+        pagelist += pagedata
 
 
 url = 'https://8ch.net/log.php?board=pol'
@@ -29,11 +47,10 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 pagelist = processmodactions(soup)
 otherpages = listoflinks(soup)
-for page in otherpages:
-    url = page
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    pagelist += processmodactions(soup)
-print("mod actions: " + str(len(pagelist)) + ", first timestamp: " + pagelist[0].get("timestamp") + "last timestamp: " + pagelist[-1].get("timestamp"))
+timestring = time.strftime("%Y:%m:%d-%H:%M:%S")
+filename = "scrapefile" + timestring + ".txt"
+processlinks(otherpages, pagelist)
+
+print("mod actions: " + str(len(pagelist)) + ", first timestamp: " + pagelist[0].get("timestamp") + " last timestamp: " + pagelist[-1].get("timestamp"))
 
 
